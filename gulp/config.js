23037548a859ddeb304905906feb78d5796ecb38
Moore.Huang <moore@moorehy.com>
2018-04-02 22:23:02 +0800

    M: 修改为使用bootstrap.bundle.js

--- gulp/config.js
+++ gulp/config.js
@@ -148,7 +148,7 @@ module.exports = {
       js: {
         src: [
           node_modules + '/jquery/dist/jquery.js',
-          node_modules + '/bootstrap/dist/js/bootstrap.js',
+          node_modules + '/bootstrap/dist/js/bootstrap.bundle.js',
           node_modules + '/vue/dist/vue.js',
         ],
         dest: developmentAssets + '/js',
