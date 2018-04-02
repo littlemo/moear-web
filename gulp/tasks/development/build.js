e1f6be2a97fa7ed7893efbbd4427f3e77c52c920
Moore.Huang <moore@moorehy.com>
2018-04-02 10:56:07 +0800

    F: 实现build任务，并预先添加delete任务调用

---
+++ gulp/tasks/development/build.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+
+/**
+ * 根据定义顺序执行全部构建任务
+ */
+gulp.task('build', function(callback) {
+  runSequence(
+    'delete',
+    callback);
+});
