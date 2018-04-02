940dda87fad971b8af4cbc3fbc324705f8bcdebb
Moore.Huang <moore@moorehy.com>
2018-04-02 11:40:47 +0800

    F: 移植常用的PostCSS编译器到styles任务，并设置相关config

--- gulp/config.js
+++ gulp/config.js
@@ -84,6 +84,26 @@ module.exports = {
       }
     }
   },
+  styles: {
+    src: srcAssets + '/styles/*.css',
+    dest: developmentAssets + '/css',
+    options: {
+      autoprefixer: {
+        browsers: [
+          'last 2 versions',
+          'safari 5',
+          'ie 8',
+          'ie 9',
+          'opera 12.1',
+          'ios 6',
+          'android 4',
+        ],
+        cascade: true
+      },
+      mqpacker: {},
+      cssnano: {}
+    }
+  },
   relyonJS: {
     src: [
       srcAssets + '/static/js/*.js',
---
+++ gulp/tasks/development/styles.js
@@ -0,0 +1,48 @@
+var gulp           = require('gulp');
+var postcss        = require('gulp-postcss');
+var plumber        = require('gulp-plumber');
+var sourcemaps     = require('gulp-sourcemaps');
+var size           = require('gulp-size');
+var gutil          = require('gulp-util');
+var browsersync    = require('browser-sync');
+var atimport       = require('postcss-import')
+var nesting        = require('postcss-nesting')
+var autoprefixer   = require('autoprefixer');
+var colorfunction  = require('postcss-color-function')
+var cssvariables   = require('postcss-css-variables');
+var mqpacker       = require('css-mqpacker');
+var config         = require('../../config').styles;
+
+
+function onError (err) {
+  gutil.beep();
+  console.log(err);
+  this.emit('end');
+}
+
+/**
+ * 通过 PostCSS 和它的插件运行 CSS
+ * 构建 sourcemaps 并进行压缩
+ */
+var processors = [
+  atimport(),
+  nesting(),
+  autoprefixer(config.options.autoprefixer),
+  cssvariables(),
+  colorfunction(),
+  mqpacker(config.options.mqpacker),
+];
+
+gulp.task('styles', function() {
+  browsersync.notify('正在编译 CSS');
+
+  return gulp.src(config.src)
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(config.dest))
+    .pipe(browsersync.stream());
+});
