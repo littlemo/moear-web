5a45295450e337ccfac3b18cd8198d2e30f51487
Moore.Huang <moore@moorehy.com>
2018-04-06 22:33:32 +0800

    F: 实现生产build任务，具体子任务待实现
    核心功能为对开发build任务输出的文件进行再处理

---
+++ gulp/tasks/production/build.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+
+/**
+ * 按定义顺序执行全部构建任务
+ */
+gulp.task('build:prod', ['build'], function(callback) {
+  runSequence(
+    'delete:prod',
+    [
+      'html:prod',
+      'optimize:css',
+      'optimize:js',
+      'optimize:images'
+    ],
+    callback);
+});
